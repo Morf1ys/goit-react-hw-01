f031095853bdfc1f819550d7064a09e4b7f7c077
Dobrota <dobrota.art@gmail.com>
2024-03-19 01:57:41 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './index.module.css';
-import  Profile  from './Componentss/Profile/Profile.jsx'
-import userData from './Componentss/Profile/UserData.json'
-import FriendList from './Componentss/FriendList/FriendList.jsx'
-import friendsData from './Componentss/FriendList/friends.json';
-import TransactionHistory from './Componentss/TransactionHistory/TransactionHistory.jsx'
-import transactions from "./Componentss/TransactionHistory/transactions.json"
+import  Profile  from './Components/Profile/Profile.jsx'
+import userData from './Components/Profile/UserData.json'
+import FriendList from './Components/FriendList/FriendList.jsx'
+import friendsData from './Components/FriendList/friends.json';
+import TransactionHistory from './Components/TransactionHistory/TransactionHistory.jsx'
+import transactions from "./Components/TransactionHistory/transactions.json"
 export default function App() {
   return (
     <>
